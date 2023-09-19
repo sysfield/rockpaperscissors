1ffb3c74acca12d9f20f072e5e2f56871bc787cc
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-19 04:00:06 +0100

    Fix array overflow in function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice()
 {
     var x = ["rock", "paper", "scissors"];
-    console.log(x[Math.round((Math.random()*10)%3)]);
+    console.log(x[Math.round((Math.random()*10)%2)]);
 }
 
 getComputerChoice();
